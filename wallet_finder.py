d602a089d180df28956e6a2122d70a7628c64b0a
EZpsychedelic <EZpsychedelic@users.noreply.github.com>
2020-05-21 15:00:49 -0400

    Imported more modules. Cleaned up code

--- wallet_finder.py
+++ wallet_finder.py
@@ -32,26 +32,31 @@
 
 import jarray
 import inspect
+from java.lang import Class
 from java.lang import System
+from java.sql  import DriverManager, SQLException
 from java.util.logging import Level
-from org.sleuthkit.datamodel import SleuthkitCase
-from org.sleuthkit.datamodel import AbstractFile
-from org.sleuthkit.datamodel import ReadContentInputStream
-from org.sleuthkit.datamodel import BlackboardArtifact
-from org.sleuthkit.datamodel import BlackboardAttribute
+from java.io import File
+from java.util import ArrayList
 from org.sleuthkit.autopsy.ingest import IngestModule
 from org.sleuthkit.autopsy.ingest.IngestModule import IngestModuleException
 from org.sleuthkit.autopsy.ingest import DataSourceIngestModule
-from org.sleuthkit.autopsy.ingest import FileIngestModule
 from org.sleuthkit.autopsy.ingest import IngestModuleFactoryAdapter
+from org.sleuthkit.autopsy.ingest import IngestModuleIngestJobSettings
+from org.sleuthkit.autopsy.ingest import IngestModuleIngestJobSettingsPanel
 from org.sleuthkit.autopsy.ingest import IngestMessage
 from org.sleuthkit.autopsy.ingest import IngestServices
+from org.sleuthkit.autopsy.ingest import ModuleDataEvent
 from org.sleuthkit.autopsy.coreutils import Logger
+from org.sleuthkit.autopsy.coreutils import PlatformUtil
 from org.sleuthkit.autopsy.casemodule import Case
 from org.sleuthkit.autopsy.casemodule.services import Services
 from org.sleuthkit.autopsy.casemodule.services import FileManager
-from org.sleuthkit.autopsy.casemodule.services import Blackboard
-
+from org.sleuthkit.autopsy.datamodel import ContentUtils
+from org.sleuthkit.datamodel import CommunicationsManager
+from org.sleuthkit.datamodel import Relationship
+from org.sleuthkit.datamodel import Account
+from org.sleuthkit.datamodel import AbstractFile
 
 # Factory that defines the name and details of the module and allows Autopsy
 # to create instances of the modules that will do the analysis.
@@ -90,6 +95,7 @@ class CryptoWalletDataSourceIngestModule(DataSourceIngestModule):
     # 'context' is an instance of org.sleuthkit.autopsy.ingest.IngestJobContext.
     # See: http://sleuthkit.org/autopsy/docs/api-docs/latest/classorg_1_1sleuthkit_1_1autopsy_1_1ingest_1_1_ingest_job_context.html
     # TODO: Add any setup code that you need here.
+
     def startUp(self, context):
 
         # Throw an IngestModule.IngestModuleException exception if there was a problem setting up
@@ -102,6 +108,7 @@ class CryptoWalletDataSourceIngestModule(DataSourceIngestModule):
     # 'progressBar' is of type org.sleuthkit.autopsy.ingest.DataSourceIngestModuleProgress
     # See: http://sleuthkit.org/autopsy/docs/api-docs/latest/classorg_1_1sleuthkit_1_1autopsy_1_1ingest_1_1_data_source_ingest_module_progress.html
     # TODO: Add your analysis code in here.
+
     def process(self, dataSource, progressBar):
 
         # we don't know how much work there is yet
@@ -128,8 +135,8 @@ class CryptoWalletDataSourceIngestModule(DataSourceIngestModule):
                 return IngestModule.ProcessResult.OK
 
             self.log(Level.INFO, "Processing file: " + file.getName())
-            fileCount += 1
 
+            fileCount += 1
             # Make an artifact on the blackboard.  TSK_INTERESTING_FILE_HIT is a generic type of
             # artfiact.  Refer to the developer docs for other examples.
             # TODO Make a more interesting artfiact
@@ -144,7 +151,8 @@ class CryptoWalletDataSourceIngestModule(DataSourceIngestModule):
                 self.log(Level.SEVERE, "Error indexing artifact " + art.getDisplayName())
 
             # To further the example, this code will read the contents of the file and count the number of bytes
-            """
+
+
             inputStream = ReadContentInputStream(file)
             buffer = jarray.zeros(1024, "b")
             totLen = 0
@@ -153,14 +161,13 @@ class CryptoWalletDataSourceIngestModule(DataSourceIngestModule):
                 totLen = totLen + readLen
                 readLen = inputStream.read(buffer)
 
-            """
+
             # Update the progress bar
             progressBar.progress(fileCount)
 
-
         #Post a message to the ingest messages in box.
-        message = IngestMessage.createMessage(IngestMessage.MessageType.DATA,
-            "Wallet Finder Module", "Found %d files" % fileCount)
-        IngestServices.getInstance().postMessage(message)
+            message = IngestMessage.createMessage(IngestMessage.MessageType.DATA,
+                "Wallet Finder Module", "Found %d files" % fileCount)
+            IngestServices.getInstance().postMessage(message)
 
         return IngestModule.ProcessResult.OK
